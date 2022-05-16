94c234e0df290767430c289924939a76eff9a560
RafoTorosyan04 <rafo2004torosyan04@gmail.com>
2022-05-16 20:19:59 +0400

    ArrayExample

---
+++ src/homework/ArrayExample.java
@@ -0,0 +1,58 @@
+package homework;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+//        int[] numbers = {2,5,8,10,32,22,11,66,12,30};
+//        for (int i = 0; i <numbers.length; i++) {
+//            System.out.print(numbers[i] + " ");
+//
+//        }
+//        int[] numbers = {2,5,8,10,32,22,11,66,12,30};
+//       int i = numbers.length -10;
+//        System.out.println(numbers[i] );
+
+//
+//        int[] numbers = {2,5,8,10,32,22,11,66,12,30};
+//        int i = numbers.length -1;
+//        System.out.println(numbers[i]);
+//
+//
+//        int[] numbers = new int[10];
+//        numbers [0] = 2;
+//        numbers [1] = 5;
+//        numbers [2] = 8;
+//        numbers [3] = 10;
+//        numbers [4] = 32;
+//        numbers [5] = 22;
+//        numbers [6] = 11;
+//        numbers [7] = 66;
+//        numbers [8] = 12;
+//        numbers [9] = 30;
+//        System.out.print(numbers[1]+" " );
+//        System.out.print(numbers[5]+" " );
+//        System.out.print(numbers[9]+" " );
+//
+//
+//        int[] numbers = new int[10];
+//        numbers [0] = 2;
+//        numbers [1] = 5;
+//        numbers [2] = 8;
+//        numbers [3] = 10;
+//        numbers [4] = 32;
+//        numbers [5] = 22;
+//        numbers [6] = 11;
+//        numbers [7] = 66;
+//        numbers [8] = 12;
+//        numbers [9] = 30;
+//        System.out.print(numbers[0]+" " );
+//        System.out.print(numbers[2]+" " );
+//        System.out.print(numbers[3]+" " );
+//        System.out.print(numbers[4]+" " );
+//        System.out.print(numbers[5]+" " );
+//        System.out.print(numbers[7]+" " );
+//        System.out.print(numbers[8]+" " );
+//
+//
+    }
+}
+
