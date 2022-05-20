dba63e2c0b57c8578b33a1404bb4a1725afcb44d
RafoTorosyan04 <rafo2004torosyan04@gmail.com>
2022-05-20 16:44:20 +0400

    CharUtil

---
+++ src/homework/CharUtil.java
@@ -0,0 +1,40 @@
+package homework;
+
+public class CharUtil {
+    public static void main(String[] args) {
+        char[] chars = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e'};
+        System.out.print( chars[3]);
+        System.out.print( chars[4]);
+
+
+
+        System.out.println();
+
+
+
+        //2
+        char[]chars1={'j','a','v','a','l','o','l','y'};
+        boolean a;
+        a=true;
+        System.out.println(" l " + a);
+        a=false;
+        System.out.println(" y " + a);
+
+        System.out.println();
+
+
+
+        //3
+        char[]bobAray={'b','a','b','o','l','a'};
+        boolean b;
+        b=true;
+        System.out.println(" bob " + b);
+
+        System.out.println();
+
+
+        //4
+        char[] text = {'.','b', 'a', 'r', 'e', 'v', '.'};
+        System.out.println(text);
+    }
+}
