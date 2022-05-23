e6d438905c0648a7f46418a5169ca1e42da63eaa
RafoTorosyan04 <rafo2004torosyan04@gmail.com>
2022-05-23 17:30:02 +0400

    ArrayUtil

---
+++ src/homework/ArrayUtil.java
@@ -0,0 +1,39 @@
+package homework;
+
+public class ArrayUtil {
+    public static void main(String[] args) {
+        int a, b, v, d, e, f, g, h;
+        a = 44;
+        b = 55;
+        if (a < b) System.out.print(a + " ");
+        v = 65;
+        d = 34;
+        if (v > d) System.out.print(v + " ");
+        e = 22;
+        f = 78;
+        if (e < f) System.out.print(f + " ");
+        g = 90;
+        h = 45;
+        if (g > h) System.out.println(g);
+
+
+
+
+
+
+
+        //2
+        char[] spaceArray={'c','a','t',' ',' ','b','i','b',' ',' '};
+        for (char c1 : spaceArray) {
+            if (c1 !=' ');{
+                System.out.print(c1);
+            }
+
+        }
+
+
+
+
+
+    }
+}
